da83df0fb306cfb262694298dc78cfbb1b6030f7
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-03-27 10:51:13 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DialogueSeries from './DialogueSeries';
+
+export default function App() {
+  return (
+    <div className="relative min-h-screen">
+      <DialogueSeries />
+    </div>
+  );
+}
